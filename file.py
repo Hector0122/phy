fdab4d07a3241e730094268466185d507a8326a5
Hector Paramo Velasquez <hector.paramo@g-global.com>
2024-10-15 14:16:05 -0700

    Refactor code and add data science steps documentation

--- file.py
+++ file.py
@@ -4,6 +4,17 @@
 # instalar IPython (pip3 install IPython)
 # instalar scikit-learn (pip3 install scikit-learn)
 
+'''
+    Etapas de Data Science 
+
+    1. Definir el problema ( que queremos estimar o predecir)
+    2. Adquirir y preparar los datos (que recursos tenemos para obtener los datos)
+    3. Explorar los datos (visualizar los datos, Localizar posibles tendencias)
+    4. Modelar los datos (crear un modelo que se ajuste a los datos y evaluarlo)
+    5. Resultados ( que resultados hemos obtenido y que podemos hacer con ellos)
+    
+'''
+
 PI = 3.1416
 
 arr = [1, 2, 3, 4, 5]
